a13814b83bab9498f2e8c30237da4994c0d604e7
Muhammad Bilal <mbilalsoomroo@gmail.com>
2024-09-18 21:43:25 +0500

    install react js tailwind css and pages create and routes

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes,Route } from "react-router-dom"
+import Home from "./components/ui/Pages/Home"
+import About from "./components/ui/Pages/About"
+import SignIn from "./components/ui/Pages/SignIn"
+import SignOut from "./components/ui/Pages/SignOut"
+import Profile from "./components/ui/Pages/Profile"
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signout" element={<SignOut />} />
+        <Route path="/Profile" element={<Profile />} />
+        
+      </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
