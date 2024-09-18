a426447090909f4257a95056f8608920f381b9fc
Muhammad Bilal <mbilalsoomroo@gmail.com>
2024-09-18 22:24:03 +0500

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import About from "./components/ui/Pages/About"
 import SignIn from "./components/ui/Pages/SignIn"
 import SignOut from "./components/ui/Pages/SignOut"
 import Profile from "./components/ui/Pages/Profile"
+import Header from "./Component/Header"
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
-      
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
