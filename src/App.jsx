bf6a8adb42e7913853b44f33f39430f17a83602e
Muhammad Bilal <mbilalsoomroo@gmail.com>
2024-09-20 12:49:41 +0500

    complete signup page ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { BrowserRouter, Routes,Route } from "react-router-dom"
 import Home from "./components/ui/Pages/Home"
 import About from "./components/ui/Pages/About"
 import SignIn from "./components/ui/Pages/SignIn"
-import SignOut from "./components/ui/Pages/SignOut"
 import Profile from "./components/ui/Pages/Profile"
 import Header from "./Component/Header"
+import SignUp from "./components/ui/Pages/SignUp"
 
 function App() {
 
@@ -16,7 +16,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/signin" element={<SignIn />} />
-        <Route path="/signout" element={<SignOut />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/Profile" element={<Profile />} />
         
       </Routes>
---
+++ src/components/ui/Pages/SignUp.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function SignUp() {
+  return (
+    <div className='p-3 max-w-lg mx-auto '>
+      <h1 className='text-3xl font-semibold text-center my-7'>Sign Up</h1>
+      <form className='flex flex-col gap-4'>
+        <input type='text' placeholder='Username'  className='border p-3 rounded-lg' id='username'/>
+        <input type='email' placeholder='Email'  className='border p-3 rounded-lg' id='email'/>
+        <input type='Password' placeholder='Password'  className='border p-3 rounded-lg' id='password'/>
+        <button className='bg-slate-900 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-85'>Sign Up</button>
+      </form>
+      <div className='flex gap-3 mt-5'>
+        <p>Have an account?</p>
+        <Link to='/signin'>
+        <span className='text-blue-700'>Sign In</span>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default SignUp
